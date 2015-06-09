b90b6679db063c65673b38303e89eeae0a753fb3
lighter <unknownlighter@gmail.com>
2015-06-09 16:34:28 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='userlayers',
     version='0.0.1',
     packages=['userlayers'],
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[
         'django-mutant == 0.1.2',
         'django-tastypie >= 0.12.0'
