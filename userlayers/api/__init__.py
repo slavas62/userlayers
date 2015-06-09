e029af327c79a4e5e4c90edf38b00beb364dedec
lighter <unknownlighter@gmail.com>
2015-06-09 19:29:38 +0300

    add proxy resource for dynamic tables

--- userlayers/api/__init__.py
+++ userlayers/api/__init__.py
@@ -1,6 +1,7 @@
 from tastypie.api import Api
-from .resources import TablesResource, FieldsResource
+from .resources import TablesResource, FieldsResource, TableProxyResource
 
 v1_api = Api(api_name='v1')
 v1_api.register(TablesResource())
 v1_api.register(FieldsResource())
+v1_api.register(TableProxyResource())
--- userlayers/api/resources.py
+++ userlayers/api/resources.py
@@ -1,8 +1,10 @@
 import mutant
 
-from tastypie.resources import ModelResource
-from tastypie import fields
-from tastypie.authorization import DjangoAuthorization
+from django.conf.urls import url
+from tastypie.resources import ModelResource, Resource
+from tastypie import fields, http
+from tastypie.authorization import DjangoAuthorization, Authorization
+from tastypie.utils import trailing_slash
 from mutant.models import ModelDefinition, FieldDefinition
 
 FIELD_TYPES = (
@@ -50,3 +52,29 @@ class TablesResource(ModelResource):
         for f in bundle.data['fields']:
             f.obj.model_def = bundle.obj
         return super(TablesResource, self).save_m2m(bundle)
+
+class TableProxyResource(Resource):
+    pattern = '^tables/(?P<table_pk>)/data'
+    
+    class Meta:
+        pass
+    
+    def dispatch(self, request_type, request, **kwargs):
+        table_pk = kwargs.pop('table_pk')
+        try:
+            md = ModelDefinition.objects.get(pk=table_pk)
+        except ModelDefinition.DoesNotExist:
+            return http.HttpNotFound()
+        
+        class R(ModelResource):
+            class Meta:
+                queryset = md.model_class().objects.all()
+                authorization = Authorization()
+        
+        return R().dispatch(request_type, request, **kwargs)
+    
+    def base_urls(self):
+        return [
+            url(r"^tablesdata/(?P<table_pk>\d+)/data%s$" % trailing_slash(), self.wrap_view('dispatch_list'), name="api_dispatch_list"),
+            url(r"^tablesdata/(?P<table_pk>\d+)/data/(?P<%s>.*?)%s$" % (self._meta.detail_uri_name, trailing_slash()), self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+        ]
