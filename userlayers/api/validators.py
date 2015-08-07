c216a19890ec0ced459781f5cf3c7e2b2284b0dd
lighter <unknownlighter@gmail.com>
2015-08-07 10:47:28 +0300

    fix file import error

--- userlayers/api/validators.py
+++ userlayers/api/validators.py
@@ -4,7 +4,7 @@ from .naming import get_app_label_for_user
 
 class FieldValidation(Validation):
     def is_valid(self, bundle, request=None):
-        if bundle.obj.model_def.fielddefinitions.filter(name=bundle.data['name']):
+        if hasattr(bundle.obj, 'model_def') and bundle.obj.model_def.fielddefinitions.filter(name=bundle.obj.name):
             return {'name': u'field with name "%s" already exists' % bundle.data['name']}
         return {}
 
