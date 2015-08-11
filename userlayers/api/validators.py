fd5c24989395d0a534e496b27a1e24f5fb026912
lighter <unknownlighter@gmail.com>
2015-08-11 15:52:22 +0300

    fix exception on duplicate geometry field

--- userlayers/api/validators.py
+++ userlayers/api/validators.py
@@ -5,7 +5,7 @@ from .naming import get_app_label_for_user
 class FieldValidation(Validation):
     def is_valid(self, bundle, request=None):
         if hasattr(bundle.obj, 'model_def') and bundle.obj.model_def.fielddefinitions.filter(name=bundle.obj.name):
-            return {'name': u'field with name "%s" already exists' % bundle.data['name']}
+            return {'name': u'field with name "%s" already exists' % bundle.obj.name}
         return {}
 
 class TableValidation(Validation):
