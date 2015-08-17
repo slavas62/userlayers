af86a4aa534983754fcc267c5dd924dccaaeb4dd
Alexey Baranov <baranov@digitalcore.ru>
2015-08-17 04:30:43 +0300

    api.forms add new fields types

--- userlayers/api/forms.py
+++ userlayers/api/forms.py
@@ -1,13 +1,39 @@
 import mutant
-
 from django import forms
-
-from mutant.models import ModelDefinition, FieldDefinition
 
 FIELD_TYPES = (
     ('text', mutant.contrib.text.models.TextFieldDefinition),
+    ('varchar', mutant.contrib.text.models.CharFieldDefinition),
+
     ('integer', mutant.contrib.numeric.models.BigIntegerFieldDefinition),
-    ('boolean', mutant.contrib.boolean.models.NullBooleanFieldDefinition),
+    ('small_integer', mutant.contrib.numeric.models.SmallIntegerFieldDefinition),
+
+    ('null_boolean', mutant.contrib.boolean.models.NullBooleanFieldDefinition),
+    ('boolean', mutant.contrib.boolean.models.BooleanFieldDefinition),
+
+    ('file', mutant.contrib.file.models.FilePathFieldDefinition),
+
+    ('foreign_key', mutant.contrib.related.models.ForeignKeyDefinition),
+    ('one_to_one', mutant.contrib.related.models.OneToOneFieldDefinition),
+    ('many_to_many', mutant.contrib.related.models.ManyToManyFieldDefinition),
+
+    ('ip_generic', mutant.contrib.web.models.GenericIPAddressFieldDefinition),
+    ('ip', mutant.contrib.web.models.IPAddressFieldDefinition),
+    ('email', mutant.contrib.web.models.EmailFieldDefinition),
+    ('url', mutant.contrib.web.models.URLFieldDefinition),
+
+    ('geometry', mutant.contrib.geo.models.GeometryFieldDefinition),
+    ('point', mutant.contrib.geo.models.PointFieldDefinition),
+    ('multi_point', mutant.contrib.geo.models.MultiPointFieldDefinition),
+    ('line_string', mutant.contrib.geo.models.LineStringFieldDefinition),
+    ('multi_line_string', mutant.contrib.geo.models.MultiLineStringFieldDefinition),
+    ('polygon', mutant.contrib.geo.models.PolygonFieldDefinition),
+    ('multi_polygon', mutant.contrib.geo.models.MultiPolygonFieldDefinition),
+    ('geometry_collection', mutant.contrib.geo.models.GeometryCollectionFieldDefinition),
+
+    ('date', mutant.contrib.temporal.models.DateFieldDefinition),
+    ('time', mutant.contrib.temporal.models.TimeFieldDefinition),
+    ('datetime', mutant.contrib.temporal.models.DateTimeFieldDefinition),
 )
 
 class FieldForm(forms.ModelForm):
@@ -15,8 +41,8 @@ class FieldForm(forms.ModelForm):
     table = forms.CharField(required=False)
     
     class Meta:
-        model = FieldDefinition
-        fields = ['name',]
+        model = mutant.models.FieldDefinition
+        fields = ['name']
 
     def clean_table(self):
         return self.cleaned_data['table'] or None 
