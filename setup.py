cef3b1b96a1acd6550af49994d7b5297918a3b43
nerox <gneroks@gmail.com>
2016-01-11 11:40:11 +0300

    update django-mutant

--- setup.py
+++ setup.py
@@ -6,14 +6,13 @@ setup(
     packages=find_packages(),
     long_description=open('README.md').read(),
     install_requires=[
-        'django-mutant == 0.2.0a',
+        'django-mutant == 0.2',
         'django-tastypie >= 0.12.0',
         'transliterate == 1.7.3',
         'vectortools == 0.0.4',
         'shapeutils == 0.0.1',
     ],
     dependency_links = [
-        'https://github.com/charettes/django-mutant/archive/83a09a6.zip#egg=django-mutant-0.2.0a',
         'https://bitbucket.org/lighter/shape-utils/get/eec0952.zip#egg=shapeutils-0.0.1',
     ],
 )
