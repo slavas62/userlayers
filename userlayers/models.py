82f40424d5ca507d40cfa95bbce10434b5966847
slavas <slavas@bk.ru>
2020-09-07 17:19:38 +0300

    print

--- userlayers/models.py
+++ userlayers/models.py
@@ -17,5 +17,6 @@ attached_file_upload_to = UuidHashFilenameUploadTo('attached_files/')
 class AttachedFile(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
+    print('------------------------------------------------------------------------------------')
     file = models.FileField(upload_to=attached_file_upload_to)
     object = GenericForeignKey()
