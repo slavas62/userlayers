1c2920c39a5e31d2b7ce088a2ac5f47b056d5e01
slavas <slavas@bk.ru>
2020-09-09 10:40:14 +0300

    Меняем файлы на файлы из виртуальной среды разработки ГИП Геотрон /home/slavas/MyProjects/geotron/lib/python2.7/site-packages/userlayers/

--- userlayers/models.py
+++ userlayers/models.py
@@ -17,6 +17,5 @@ attached_file_upload_to = UuidHashFilenameUploadTo('attached_files/')
 class AttachedFile(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    print('------------------------------------------------------------------------------------')
     file = models.FileField(upload_to=attached_file_upload_to)
     object = GenericForeignKey()
